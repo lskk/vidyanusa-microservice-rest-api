a1157981e00894e374b903c8f07b93840ee77736
emilhamep <emilhamep@gmail.com>
2017-04-28 06:54:24 +0700

    Penambahan API masuk/login

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,36 @@ router.get('/daftar_sekolah', function(req, res, next) {
 
 });
 
+router.post('/masuk', function(req, res, next) {
+  console.log(req.body.username);
+  console.log(req.body.password);
 
+  co(function*() {
+    // Connection URL
+    var url = 'mongodb://localhost:27017/vidyanusa';
+    // Use connect method to connect to the Server
+    var db = yield MongoClient.connect(url);
+    //console.log('Berhasil koneksi ke database');
+
+    // Find the collection users
+    var col = db.collection('users');
+    var docs = yield col.find({username: req.body.username , password: req.body.password}).toArray();
+
+    // Close the connection
+    db.close();
+
+    res.json({status: '00' , message: 'success', data: docs, proceed: 1});
+
+  }).catch(function(err) {
+    console.log(err.stack);
+
+    // Close the connection
+    db.close();
+
+    res.json({status: '01' , message: 'failed'});
+  });
+
+
+});
 
 module.exports = router;
