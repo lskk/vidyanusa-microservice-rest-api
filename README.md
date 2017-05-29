882503352b1043a0409b6a62589117234c4b921f
emilhamep <emilhamep@gmail.com>
2017-05-29 11:08:37 +0700

    .gitignore is now working

--- README.md
+++
@@ -1,2 +0,0 @@
-# vidyanusa-microservice-rest-api
-Microservice ini berperan sebagai penyedia layanan REST API ke layer 1 atau Database(Mongo).
--- routes/schemas/schools.js
+++
@@ -1,8 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var SchoolsSchema   = new Schema({
-    name: String
-});
-
-module.exports = mongoose.model('Bear', SchoolsSchema);
